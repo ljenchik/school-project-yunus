014a25d1437ca433102276f76e0cb69fcd33ad87
ljenchik <pelagenkolena@gmail.com>
2024-01-28 22:23:33 +0000

    Minor changes

--- src.c
+++ src.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#include<stdbool.h>
+#include <string.h>
+#include <stdbool.h>
 #define max_number_of_subjects 10
 
 #include "subject.h"
 #include "teacher.h"
 #include "student.h"
 
-int main() 
+int main()
 
- {
+{
   StudentList *student_head = create_student();
   StudentList *new_student1 = create_student();
   StudentList *new_student2 = create_student();
-  
+
   // Adding students to StudentList
   add_student(student_head, student_head);
   add_student(student_head, new_student1);
@@ -24,20 +24,14 @@ int main()
   print_students(student_head);
 
   ArrayList *available_subjects = malloc(sizeof(ArrayList));
-  available_subjects->allocated_count = 10;
-  
-   available_subjects->current_subject_list[0] = malloc(sizeof(Subject));
-   Subject *new_subject1 = createSubject();
- 
-   available_subjects->current_subject_list[1] = malloc(sizeof(Subject));
-   Subject *new_subject2 = createSubject();
 
-   available_subjects->current_subject_list[2] = malloc(sizeof(Subject));
-   Subject *new_subject3 = createSubject();
+  Subject *new_subject1 = createSubject();
+  Subject *new_subject2 = createSubject();
+  Subject *new_subject3 = createSubject();
 
-   add_subject(available_subjects, new_subject1);
-   add_subject(available_subjects, new_subject2);
-   add_subject(available_subjects, new_subject3);
+  add_subject(available_subjects, new_subject1);
+  add_subject(available_subjects, new_subject2);
+  add_subject(available_subjects, new_subject3);
 
   print_subjects(available_subjects);
 
@@ -49,30 +43,26 @@ int main()
   add_subject_to_student(&(new_student2->student), new_subject3);
   add_subject_to_student(&(new_student2->student), new_subject2);
 
+  print_students(student_head);
+
   // Creating teachers
   Teacher *t1 = createTeacher();
   Teacher *t2 = createTeacher();
-
-  printf("=======================================\n");
-  printf("=======================================\n");
-  
-  print_students(student_head);
-
-  printf("=======================================\n");
-  printf("=======================================\n");
 
   printf("Teacher id\tTeacher name\tSubject id\n");
   print_teacher(t1);
   print_teacher(t2);
 
-  printf("=======================================\n");
-  printf("=======================================\n");
-
-
   add_grade(student_head, 1, 1, 98);
+  add_grade(student_head, 1, 2, 86);
+
+  add_grade(student_head, 2, 2, 34);
+  add_grade(student_head, 3, 3, 100);
 
   printf("Subject\t\tGrade\n");
   print_subjects_with_grades(&student_head->student);
+  print_subjects_with_grades(&new_student1->student);
+  print_subjects_with_grades(&new_student2->student);
 
-   return 0;
+  return 0;
 }
--- student.h
+++ student.h
@@ -9,43 +9,47 @@
 
 #include "subject.h"
 
-typedef struct student 
+typedef struct student
 {
   int id;
   char name[20];
   ArrayList *subjects;
 } Student;
 
-typedef struct student_list {
-	Student student;
+typedef struct student_list
+{
+  Student student;
   struct student_list *next;
 } StudentList;
 
-
 // Creating students using a user's input
-StudentList *create_student() {
-  StudentList *new_student=(StudentList*)malloc(sizeof(StudentList));
-  new_student->student.subjects = (ArrayList*) malloc(sizeof(ArrayList));
+StudentList *create_student()
+{
+  StudentList *new_student = (StudentList *)malloc(sizeof(StudentList));
+  new_student->student.subjects = (ArrayList *)malloc(sizeof(ArrayList));
   memset(new_student->student.subjects, 0, sizeof(ArrayList));
   printf("Enter student id: ");
   scanf("%d", &new_student->student.id);
   printf("Enter student name: ");
   scanf("%s", new_student->student.name);
-  new_student->next=NULL;
+  new_student->next = NULL;
   return new_student;
 }
 
 // Printing a given student with all info
-void print_student(StudentList *student) {
+void print_student(StudentList *student)
+{
   printf("\t%d\t%s\t\t", student->student.id, student->student.name);
-  for (int i = 0; i < student->student.subjects->count; i++) {
-    printf("%s ", student->student.subjects->current_subject_list[i]->name);
+  for (int i = 0; i < student->student.subjects->count; i++)
+  {
+    printf("%s ", student->student.subjects->current_subject_list[i].name);
   }
   printf("\n");
 }
 
-// Adding a new student 
-void add_student(StudentList *head, StudentList *new_student) {
+// Adding a new student
+void add_student(StudentList *head, StudentList *new_student)
+{
   if (new_student == NULL)
   {
     printf("Error: invalid pointer, no student to add \n");
@@ -54,27 +58,32 @@ void add_student(StudentList *head, StudentList *new_student) {
   {
     head = new_student;
   }
-  else {
+  else
+  {
     StudentList *current = head;
-    while (current->next != NULL) {
+    while (current->next != NULL)
+    {
       current = current->next;
     }
     current->next = new_student;
-    new_student->next=NULL;
+    new_student->next = NULL;
   }
 }
 
 // Printing all students
-void print_students(StudentList *head) {
+void print_students(StudentList *head)
+{
   if (head == NULL)
   {
     printf("Error: invalid pointer \n");
   }
-  else {
+  else
+  {
     StudentList *current = head;
     printf("Student id\tName\t\tSubjects\n");
     printf("\n");
-    while (current != NULL) {
+    while (current != NULL)
+    {
       print_student(current);
       current = current->next;
     }
@@ -82,7 +91,8 @@ void print_students(StudentList *head) {
   }
 }
 
-void add_subject_to_student(Student *student, Subject *new_subject) {
+void add_subject_to_student(Student *student, Subject *new_subject)
+{
   // for (int i = 0; i < 10; i++) {
   //   if (student->subjects[i].subjectId == 0) {
   //     student->subjects[i] = *new_subject;
@@ -93,10 +103,13 @@ void add_subject_to_student(Student *student, Subject *new_subject) {
 }
 
 // Getting a student by id
-StudentList *get_student_by_id(StudentList *head, int student_id){
+StudentList *get_student_by_id(StudentList *head, int student_id)
+{
   StudentList *current = head;
-  while (current != NULL) {
-    if (current->student.id == student_id) {
+  while (current != NULL)
+  {
+    if (current->student.id == student_id)
+    {
       return current;
     }
     current = current->next;
@@ -105,22 +118,27 @@ StudentList *get_student_by_id(StudentList *head, int student_id){
 }
 
 // Add a grade to a given student to a given subject
-void add_grade(StudentList *head, int student_id, int subject_id, int grade) {
+void add_grade(StudentList *head, int student_id, int subject_id, int grade)
+{
   StudentList *found_student = get_student_by_id(head, student_id);
-  for (int i = 0; i < 10; i++) {
-    if (found_student->student.subjects->current_subject_list[i]->subjectId == subject_id) {
-      found_student->student.subjects->current_subject_list[i]->grade = grade;
+  for (int i = 0; i < 10; i++)
+  {
+    if (found_student->student.subjects->current_subject_list[i].subjectId == subject_id)
+    {
+      found_student->student.subjects->current_subject_list[i].grade = grade;
       return;
     }
   }
 }
 
-
 // Prints all subjects with the corresponding grades for a given student
-void print_subjects_with_grades(Student *student) {
-  for (int i = 0; i < 10; i++) {
-    if (student->subjects->current_subject_list[i]->subjectId != 0) {
-      printf("%s\t\t%d\n", student->subjects->current_subject_list[i]->name, student->subjects->current_subject_list[i]->grade);
+void print_subjects_with_grades(Student *student)
+{
+  for (int i = 0; i < student->subjects->count; i++)
+  {
+    if (student->subjects->current_subject_list[i].subjectId != 0)
+    {
+      printf("%s\t\t%d\n", student->subjects->current_subject_list[i].name, student->subjects->current_subject_list[i].grade);
     }
   }
 }
