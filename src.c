df31306fcf6a04be5e06971dd98dfec66dbb2419
ljenchik <pelagenkolena@gmail.com>
2024-02-19 14:19:32 +0000

    Added options for better user interface

--- src.c
+++ src.c
@@ -5,15 +5,15 @@
 int main(void)
 
 {
+  // Initial data to start with
+  printf("================= Add student ==============\n");
   StudentList *student_head = create_student_head();
   add_student(student_head, student_head);
-
   // Prompting user to add students as many as they want
   user_students_inputs(student_head);
-
-  // Printying all existing students
   print_students(student_head);
 
+  printf("================= Add subjects ==============\n");
   // Creating an array of subjects
   ArrayList *available_subjects = malloc(sizeof(ArrayList));
   if (available_subjects == NULL)
@@ -28,41 +28,70 @@ int main(void)
 
   // Prompting user to add subjects
   user_subjects_inputs(available_subjects);
-
-  // Printing all available subjects
   print_subjects(available_subjects);
 
-  // Adding subject to a student
-  user_subject_to_student(student_head, available_subjects);
-  print_students(student_head);
-
+  printf("================= Add teachers ==============\n");
   // Creating teachers
   TeacherList *teacher_head = createTeacherHead(available_subjects);
   add_teacher(teacher_head, teacher_head);
   user_teachers_inputs(teacher_head, available_subjects);
   print_teachers(teacher_head);
 
-  printf("===============================================================\n");
-  printf("===============================================================\n");
+  while (1)
+  {
+    printf("Select one of the options:\n");
+    printf("1. Add student\n");
+    printf("2. Add subject\n");
+    printf("3. Add teacher\n");
+    printf("4. Assign subject to student\n");
+    printf("5. Assign grade\n");
+    printf("6. Find students by subject\n");
+    printf("7. Find teacher by subject\n");
+    printf("8. Find grades by student and subject\n");
+    printf("0. Exit\n");
 
-  print_students(student_head);
-  print_subjects(available_subjects);
-  print_teachers(teacher_head);
+    int option;
+    printf("Enter your choice: ");
+    scanf("%d", &option);
 
-  // Adding grades
-  user_grade_to_student(student_head);
-  // Printing all students with the corresponding subjects and grades
-  print_subjects_with_grades(student_head);
-
-  // Prints all students studying a given subject
-  user_students_by_subject(student_head, available_subjects);
-
-  // Finding a teacher by a specified subject id
-  user_find_teacher_by_subject(teacher_head, available_subjects);
-
-  // Finding a grade for a given student and a given subject
-  user_find_grade(student_head, available_subjects);
-
+    switch (option)
+    {
+    case 1:
+      user_students_inputs(student_head);
+      print_students(student_head);
+      break;
+    case 2:
+      user_subjects_inputs(available_subjects);
+      print_subjects(available_subjects);
+      break;
+    case 3:
+      user_teachers_inputs(teacher_head, available_subjects);
+      print_teachers(teacher_head);
+      break;
+    case 4:
+      user_subject_to_student(student_head, available_subjects);
+      print_students(student_head);
+      break;
+    case 5:
+      user_grade_to_student(student_head);
+      print_subjects_with_grades(student_head);
+      break;
+    case 6:
+      user_students_by_subject(student_head, available_subjects);
+      break;
+    case 7:
+      user_find_teacher_by_subject(teacher_head, available_subjects);
+      break;
+    case 8:
+      user_find_grade(student_head, available_subjects);
+      break;
+    case 0:
+      printf("Exiting the program.\n");
+      return 0;
+    default:
+      printf("Invalid choice. Try again.\n");
+    }
+  }
   free(available_subjects);
 
   return 0;
